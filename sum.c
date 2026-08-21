ff93b347ffd903a53b01317d990b70c978ef148a
MahiSharma0711 <Mahi.29888@stu.upes.ac.in>
2026-08-21 15:47:20 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter the value for a");
+    scanf("%d",&a);
+    printf("Enter the value for b");
+    scanf("%d",&b);
+    printf("Sum is %d",a+b);
+}
+
